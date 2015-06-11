42fa18cf8d5030d0f81b0f5fcf4300e105919f76
Ernesto Mancilla <emancillao@gmail.com>
2015-06-11 12:42:38 -0500

    Settings

--- musiclibrary/settings.py
+++ musiclibrary/settings.py
@@ -1,34 +1,13 @@
-"""
-Django settings for musiclibrary project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '42xi-j8*3rpzb418g6kpvdr=8pbgsn#0lr9b#62cb-e9gdp&iq'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -50,13 +29,10 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'musiclibrary.urls'
-
 WSGI_APPLICATION = 'musiclibrary.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -65,23 +41,18 @@ DATABASES = {
 }
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'es-PE'
+TIME_ZONE = 'America/Lima'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../groovewolf-static/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '../groovewolf-media/')
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
